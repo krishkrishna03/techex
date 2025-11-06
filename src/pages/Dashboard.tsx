a950d25658f9887bb74032628f42cb435b44d24c
patnala sri krishna sai <96357392+krishkrishna03@users.noreply.github.com>
2025-11-06 21:32:57 +0530

    Update Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -148,6 +148,7 @@ const Dashboard: React.FC = () => {
           <Navbar
             title={getDashboardTitle()}
             onProfileClick={() => setShowProfile(true)}
+            onTabChange={setActiveTab}
           />
         )}
 
